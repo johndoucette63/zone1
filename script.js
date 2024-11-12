7d5f8705663a5f3c00c108d6d9d2df3c044061e0
John Doucette <doucette.j@gmail.com>
2024-11-12 12:46:39 -0600

    Refactor index.html to integrate Delphi page script and loader; remove obsolete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        const message = document.createElement("div");
-        message.textContent = "Taking my time to load this, cuz I am showing you that I am dynamic.";
-        document.body.appendChild(message);
-    }, 3000);
-});
