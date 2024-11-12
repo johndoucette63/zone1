f370887b850a278eca6241d18f21f2d6af070e45
John Doucette <doucette.j@gmail.com>
2024-11-12 08:37:39 -0600

    Update index.html and script.js to enhance user messaging and improve clarity

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         const message = document.createElement("div");
-        message.textContent = "Dynamic Load";
+        message.textContent = "Taking my time to load this, cuz I am showing you that I am dynamic.";
         document.body.appendChild(message);
     }, 3000);
 });
